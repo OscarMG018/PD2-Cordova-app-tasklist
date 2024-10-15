073445051fa3fc3253794dded12a32d0b27b52a3
OscarMG018 <108469203+OscarMG018@users.noreply.github.com>
2024-10-15 17:32:32 +0200

    v0.1.8

--- www/js/index.js
+++ www/js/index.js
@@ -1,14 +1,92 @@
 let items = [];
+let currentTask = null;
 
-$('#dialog').hide();
+let addDialog = $('#dialog-add').dialog({
+    resizable: false,
+    autoOpen: false,
+    width: 400,
+    height: 200,
+    title: 'Add item',
+    modal: true,
+    buttons: {
+        'Add Item': function() {
+            let item = $('#item').val();
+            if (item) {
+                addItem(item);
+                $('#item').val('');
+                $(this).dialog('close');
+            }
+        },
+        'Cancel': function() {
+            $(this).dialog('close');
+        }
+    }
+});
 
-$('#add').on('click', () => {
-    let item = prompt('Enter item');
-    addItem(item);
+let editDialog = $('#dialog-edit').dialog({
+    resizable: false,
+    autoOpen: false,
+    width: 400,
+    height: 200,
+    title: 'Edit item',
+    modal: true,
+    buttons: {
+        'Edit Item': function() {
+            let newItem = $('#new-item').val();
+            if (newItem) {
+                editItem(newItem);
+                $('#new-item').val('');
+                $(this).dialog('close');
+            }
+        },
+        'Cancel': function() {
+            $(this).dialog('close');
+        }
+    }
+});
+
+let deleteDialog = $('#dialog-delete').dialog({
+    resizable: false,
+    autoOpen: false,
+    width: 400,
+    height: 200,
+    title: 'Delete item',
+    modal: true,
+    buttons: {
+        'Delete': function() {
+            deleteItem();
+            $(this).dialog('close');
+        },
+        'Cancel': function() {
+            $(this).dialog('close');
+        }
+    }
+});
+
+
+$('#add').on('click', function() {
+    addDialog.dialog('open');
 });
 
 function addItem(item) {
-    let task = $('<div class="task"><p>' + item + '</p></div>');
+    let task = $('<li class="task"></li>');
+    let taskText = $('<p class="task-text">' + item + '</p>');
+    let editButton = $('<button class="edit button">Edit</button>').on('click', () => {
+        currentTask = task;
+        editDialog.dialog('open');
+    });
+    let deleteButton = $('<button class="delete button">Delete</button>').on('click', () => {
+        currentTask = task;
+        deleteDialog.dialog('open');
+    });
+    task.append(taskText, editButton, deleteButton);
     $('#list').append(task);
+}
 
+function editItem(newItem) {
+    currentTask.find('p').text(newItem);
+}
+
+function deleteItem() {
+    currentTask.remove();
 }
